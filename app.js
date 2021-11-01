087c90c5c87857993ad5058a37a0650a5aec1804
sakibcse570 <17201122@uap-bd.edu>
2021-11-01 13:11:02 +0600

    Added functionality: Validation

--- app.js
+++ app.js
@@ -34,6 +34,22 @@ class UI {
         document.querySelector("#author").value = '';
         document.querySelector("#isbn").value = '';
     }
+
+    showAlert(message, className) {
+        let div = document.createElement('div');
+        div.className = `alert ${className}`; //error will show in place of classname which is a built in class of skeleton CSS
+        div.appendChild(document.createTextNode(message));
+        let container = document.querySelector('.container');
+        let form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        /*setTimeout(function () {
+            document.querySelector('.alert').remove(); //we can all with error class also
+        }, 3000);*/
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000); //error message will vanish after 3 sec
+    }
 }
 
 // Add Event Listener
@@ -44,17 +60,27 @@ form.addEventListener('submit', newBook);
 function newBook(e) {
     //console.log('Hello');
     let title = document.querySelector("#title").value,
-        author = document.querySelector("#author").value,
-        isbn = document.querySelector("#isbn").value;
-
-    let book = new Book(title, author, isbn);
-    //console.log(book);
-
+    author = document.querySelector("#author").value,
+    isbn = document.querySelector("#isbn").value;
+ 
     let ui = new UI();
+     
+    if(title === '' || author === '' || isbn === '') {
+        //alert("All Fields!");
+        ui.showAlert("Please fill all the fields!", "error");
+    } else {
+        let book = new Book(title, author, isbn);
+        //console.log(book);
 
-    ui.addBooklist(book);
+        ui.addBooklist(book);
 
-    ui.clearFields();
+        ui.showAlert("Book Added!", "success");
+
+        ui.clearFields();
+    }
+    
+
+    
 
     e.preventDefault();
 }
